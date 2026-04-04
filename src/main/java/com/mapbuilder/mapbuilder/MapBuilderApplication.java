cf056c4846ea0cdf6293b7ca6afc75d1fe93cf02
nhaiderhtl <n.haider@students.htl-leonding.ac.at>
2026-04-04 23:14:41 +0200

    refactor(application): rename HelloApplication to MapBuilderApplication and update launcher

--- src/main/java/com/mapbuilder/mapbuilder/MapBuilderApplication.java
+++ src/main/java/com/mapbuilder/mapbuilder/MapBuilderApplication.java
@@ -6,7 +6,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class HelloApplication extends Application {
+public class MapBuilderApplication extends Application {
     @Override
     public void start(Stage stage) {
         MainView view = new MainView();
