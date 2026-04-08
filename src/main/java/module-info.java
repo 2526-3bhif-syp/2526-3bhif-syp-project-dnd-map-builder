ba45191e710063c3dc0216e87689771394879d66
e-reitbauer <erik.reitbauer2@gmail.com>
2026-04-08 12:53:06 +0200

    fix(module-info): remove deleted core package export

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.mapbuilder.mapbuilder {
     requires transitive javafx.graphics;
     exports com.mapbuilder.mapbuilder;
     exports com.mapbuilder.mapbuilder.main;
-    exports com.mapbuilder.mapbuilder.core;
     exports com.mapbuilder.mapbuilder.core.map;
     exports com.mapbuilder.mapbuilder.core.math;
 }
