f089b5ff9d968cfd84ad49454c70adf3e34a12fb
aHasemHTL <a.hasem@students.htl-leonding.ac.at>
2026-06-03 12:40:08 +0200

    feat(kingdom): add name property with default value and corresponding getter/setter methods

--- src/main/java/com/mapbuilder/mapbuilder/core/map/Kingdom.java
+++ src/main/java/com/mapbuilder/mapbuilder/core/map/Kingdom.java
@@ -4,14 +4,19 @@ public class Kingdom {
     private int id;
     private int colorARGB;
     private MapCell capital;
+    private String name;
 
     public Kingdom(int id, int colorARGB, MapCell capital) {
         this.id = id;
         this.colorARGB = colorARGB;
         this.capital = capital;
+        this.name = "Province " + (id + 1);
     }
 
-    public int getId() { return id; }
-    public int getColorARGB() { return colorARGB; }
-    public MapCell getCapital() { return capital; }
+    public int getId()                       { return id; }
+    public int getColorARGB()               { return colorARGB; }
+    public void setColorARGB(int colorARGB) { this.colorARGB = colorARGB; }
+    public MapCell getCapital()              { return capital; }
+    public String getName()                  { return name; }
+    public void setName(String name)         { this.name = name; }
 }
